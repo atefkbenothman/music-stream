f70ec8fca6b66741021ff4d5b37c3f09903c414a
atefkbenothman <atefkbenothman@gmail.com>
2024-11-19 23:37:28 -0800

    upload working

--- frontend/src/app/api/upload/route.ts
+++ frontend/src/app/api/upload/route.ts
@@ -1,6 +1,22 @@
 "use server"
+import fs from "fs"
+import path from "path"
 import { NextResponse } from "next/server"
 
+
+async function saveAudioFileLocally(file: File) {
+  const filePath = path.join(process.cwd(), "audio-files", file.name)
+  fs.mkdirSync(path.dirname(filePath), { recursive: true })
+  const buffer = Buffer.from(await file.arrayBuffer())
+  fs.writeFileSync(filePath, buffer)
+}
+
 export async function POST(req: Request) {
-  return NextResponse.json({ message: "file uploaded successfully" })
+  const data = await req.formData()
+  const file: File | null = data.get("file") as unknown as File
+  if (!file) {
+    return NextResponse.json( { message: "no file was uploaded" })
+  }
+  saveAudioFileLocally(file)
+  return NextResponse.json({ message: "file uploaded successfully", name: file.name })
 }
--- frontend/src/app/upload/fileInputPicker.tsx
+++ frontend/src/app/upload/fileInputPicker.tsx
@@ -1,27 +1,29 @@
 "use client"
+import axios, { AxiosProgressEvent, AxiosRequestConfig } from "axios"
 import { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/hooks/use-toast"
 import { Toaster } from "@/components/ui/toaster"
+import { Progress } from "@/components/ui/progress"
 
 
-async function uploadFile(file: File) {
-  const response = await fetch("/api/upload", {
-    method: "GET",
-  })
-  if (!response.ok) {
-    throw new Error(`upload failed with status: ${response.status}`)
+async function uploadFile(file: File, onProgress: (progress: number) => void) {
+  const formData = new FormData()
+  formData.set("file", file)
+  const options: AxiosRequestConfig = {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    },
+    onUploadProgress: (progressEvent: AxiosProgressEvent) => {
+      if (progressEvent.total) {
+        const percentage = (progressEvent.loaded * 100) / progressEvent.total
+        onProgress(percentage)
+      }
+    }
   }
-  const data = await response.json()
-  console.log("response: ", data)
-  // const response = await fetch("/api/upload", {
-  //   method: "POST",
-  // })
-  // if (!response.ok) {
-  //   throw new Error(`upload failed with status: ${response.status}`)
-  // }
-  // console.log("response: ", response)
+  const response = await axios.post("/api/upload", formData, options)
+  const data = response.data
 }
 
 
@@ -47,6 +49,8 @@ export default function FilePicker() {
   const { toast } = useToast()
 
   const [file, setFile] = useState<File | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [progressVal, setProgressVal] = useState<number>(0)
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -58,15 +62,21 @@ export default function FilePicker() {
 
   const handleUploadFile = (event: React.MouseEvent<HTMLButtonElement>) => {
     if (!file) return
-    uploadFile(file)
-    toast({title: `Uploading '${file.name}'`})
+    toast({title: `Uploading '${file.name}'...`})
+    setLoading(true)
+    uploadFile(file, setProgressVal).then(() => {
+      setLoading(false)
+      setProgressVal(0)
+      toast({title: `Succesfully uploaded '${file.name}' ✅`})
+    })
   }
 
   return (
     <div className="flex flex-col gap-4">
       <FileMetaData file={file} />
       <Input type="file" id="fileInput" onChange={handleFileChange} accept="audio/*" />
-      <Button className="bg-blue-600 hover:bg-blue-600 font-bold" disabled={!file} onClick={handleUploadFile}>Upload</Button>
+      <Button className="bg-blue-600 hover:bg-blue-600 font-bold" disabled={!file || loading} onClick={handleUploadFile}>Upload</Button>
+      <Progress value={progressVal} max={file?.size} hidden={!loading} color="red" />
       <Toaster />
     </div>
   )
